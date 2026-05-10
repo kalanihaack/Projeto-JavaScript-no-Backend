bf6e63a18993068b768e9e3b41214658242d0319
Kalani <kalanihaack@gmail.com>
2026-05-10 17:36:21 -0300

    N13 - Puxando dados de um usuario a partir do ID

--- routes/users.js
+++ routes/users.js
@@ -12,27 +12,41 @@ module.exports = app => {
 
         db.find({}).sort({ name: 1 }).exec((err, users) => {
             if (err) {
-               app.utils.error.send(err, req, res) //se der erro, retorna o erro configurado em utils 
+                app.utils.error.send(err, req, res) //se der erro, retorna o erro configurado em utils 
             } else {
                 res.statusCode = 200
                 res.setHeader("Content-Type", "application/json")
                 res.json({
-                    users
+                    users //mostra todos os usuarios
                 })
             }
         })
     })
 
-        route.post((req, res) => {
+    route.post((req, res) => {
 
-            db.insert(req.body, (err, user) => { //enviando informacoes para o banco de dado via API que retorna o id unico dele
+        db.insert(req.body, (err, user) => { //enviando informacoes para o banco de dado via API que retorna o id unico dele
 
-                if (err) {
-                    app.utils.error.send(err, req, res) //se der erro, retorna o erro configurado em utils 
-                } else {
-                    res.status(200).json(user)
-                }
-            })
+            if (err) {
+                app.utils.error.send(err, req, res) //se der erro, retorna o erro configurado em utils 
+            } else {
+                res.status(200).json(user) //se der certo, cria o usuario
+            }
         })
+    })
 
-    }
+    let routeId = app.route("/users/:id")
+
+    routeId.get((req, res) => {
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res)
+            } else {
+                res.status(200).json(user)
+            }
+        }) //rota criada para puxar o usuario a partir do id dele
+    })
+
+}
