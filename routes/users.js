da0cbf75b449fc6026f06ba4c9b943066e463d17
Kalani <kalanihaack@gmail.com>
2026-05-10 17:24:18 -0300

    N11 - Listando os usuarios a partir do banco de dados

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,42 @@
 let NeDB = require("nedb")
 let db = new NeDB({
-    filename:"users.db",
+    filename: "users.db",
     autoload: true
 })
 
-module.exports = app=>{ 
+module.exports = app => {
 
-app.get("/users", (req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content-Type", "application/json")
-    res.json({
-        users: [{
-            name: "teste",
-            email: "teste@teste.com"
-            //id: 1
-        }] //switch para retorno da /users que retorna um json de usuarios
+    app.get("/users", (req, res) => {
+
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+            if (err) {
+                console.log(`error: ${err}`)
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.statusCode = 200
+                res.setHeader("Content-Type", "application/json")
+                res.json({
+                    users
+                })
+            }
+        })
     })
- })   
 
- app.post("/users", (req, res)=>{
+        app.post("/users", (req, res) => {
 
-    db.insert(req.body, (err, user)=>{ //enviando informacoes para o banco de dado via API que retorna o id unico dele
+            db.insert(req.body, (err, user) => { //enviando informacoes para o banco de dado via API que retorna o id unico dele
 
-        if (err) {
-            console.log(`error: ${err}`)
-            res.status(400).json({
-                error : err
+                if (err) {
+                    console.log(`error: ${err}`)
+                    res.status(400).json({
+                        error: err
+                    })
+                } else {
+                    res.status(200).json(user)
+                }
             })
-        } else {
-            res.status(200).json(user)
-        }
-    })
- })
+        })
 
-}
+    }
